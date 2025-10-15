26d618483e46efb5d6da8672b2afe807ce5cca5c
Horlaolu4real <olaoluwayusuf121@gmail.com>
2025-10-15 16:53:16 +0100

    save-change

---
+++ app/api/inngest/route.js
@@ -0,0 +1,12 @@
+import { serve } from "inngest/next";
+import {
+  inngest,
+  syncUserCreation,
+  syncUserDelete,
+  syncUserUpdate,
+} from "@/config/inngest";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [syncUserCreation, syncUserUpdate, syncUserDelete],
+});
